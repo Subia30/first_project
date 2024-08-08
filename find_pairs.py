21db5831908aa6550d338c4bc876e8f5e8a45ba1
subia <skansari3098@gmail.com>
2024-08-08 22:52:45 +0530

    Hello

--- find_pairs.py
+++ find_pairs.py
@@ -1,2 +1,9 @@
-# a = [1, 1, 2, 4, 6, 2, 3, 5, 0, 9, 5, 9]
-# 
+a = [1, 1, 2, 4, 6, 2, 3, 5, 0, 9, 5, 9, 0, 0, 3, 7, 7]
+c = []
+for i in range(len(a)):
+    if a.count(a[i]) % 2 and a[i] not in c:
+        c.append(a[i])
+        
+print(c)
+if "huib":
+    print("hello")
